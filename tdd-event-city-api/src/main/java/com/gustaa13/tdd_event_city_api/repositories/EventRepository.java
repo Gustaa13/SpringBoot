4572402923931160dd6d41a11ad98bd9de0d087c
gustavo.olliveira49@gmail.com <gustavo.olliveira49@gmail.com>
2025-06-22 15:01:37 -0300

    Adicionando anotação nos repositórios

--- tdd-event-city-api/src/main/java/com/gustaa13/tdd_event_city_api/repositories/EventRepository.java
+++ tdd-event-city-api/src/main/java/com/gustaa13/tdd_event_city_api/repositories/EventRepository.java
@@ -1,9 +1,11 @@
 package com.gustaa13.tdd_event_city_api.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.gustaa13.tdd_event_city_api.entities.Event;
 
+@Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
 
 }
